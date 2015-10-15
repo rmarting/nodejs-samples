0abc60f779e11ecf300e28daf23b9cd580e39e8d
rmarting - Roman Martin Gil <rmarting@redhat.com>
2015-10-15 18:41:25 +0200

    Updated with changes in second day

--- HttpRequestSample/index.js
+++ HttpRequestSample/index.js
@@ -1,20 +1,23 @@
-var request = require("request")
+// var http = require("http");
+var request = require("request");
 
 function callMeBackAfterRequest(err, res, body) {
   if (err && res.statusCode != 200) {
-    console.log(new Date(), 'Opps! Error on HTTP call(' + res.statusCode + '): ' + err )
+    console.log(new Date(), 'Opps! Error on HTTP call(' + res.statusCode + '): ' + err );
   } else {
-    console.log(new Date(), 'HTTP Response: ' + JSON.stringify(body))
+    console.log(new Date(), 'HTTP Response: ' + JSON.stringify(body));
   }
 }
 
-var googleHttp = 'http://www.google.es'
-var openWeatherHttp = 'http://api.openweathermap.org/data/2.5/weather?q=Parla,Spain&APPID=15cb6d6a5ace533e18d6f62b34126e76&mode=json&lang=es'
+// var googleHttp = 'http://www.google.es';
+var openWeatherHttp = 'http://api.openweathermap.org/data/2.5/weather?q=Parla,Spain&APPID=15cb6d6a5ace533e18d6f62b34126e76&mode=json&lang=es';
 
-console.log(new Date(), 'Calling HTTP')
+console.log(new Date(), 'Calling HTTP');
 
-// request('http://www.google.es', callMeBackAfterRequest)
-request(openWeatherHttp, callMeBackAfterRequest)
+// request('http://www.google.es', callMeBackAfterRequest);
+// var response = http.request(openWeatherHttp, callMeBackAfterRequest);
+request(openWeatherHttp, callMeBackAfterRequest);
+// response.end();
 
-// console.log(new Date(), 'Google called')
+// console.log(new Date(), 'Google called');
 
--- ReadFileSample/index.js
+++ ReadFileSample/index.js
@@ -1,18 +1,19 @@
-var fs = require("fs")
+var fs = require("fs");
 
 // The callback we'll use
 function callbackFunctionOnceComplete (err, result) {
   if (err) {
     // Handle the error
-    console.error(new Date(), 'Error reading file: ' + err)
+    console.error(new Date(), 'Error reading file: ' + err);
   } else {
-    console.log(new Date(), 'File content:\n' + result.toString())
+    console.log(new Date(), 'File content:\n' + result.toString());
     // Do further logic
   }
 }
 
-console.log(new Date(), 'Reading file data.txt')
+console.log(new Date(), 'Reading file data.txt');
 
 fs.readFile('data.txt', callbackFunctionOnceComplete);
 
-console.log(new Date(), 'data.txt file readed')
+console.log(new Date(), 'data.txt file readed');
+
